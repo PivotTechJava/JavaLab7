bcaf6a5c6c0baae21096ec6795cdfe11a33d876c
feven mulugeta <fevenmulugeta@fevens-MacBook-Pro.local>
2021-08-09 19:21:11 -0500

    test

--- src/com/pivottech/Main.java
+++ src/com/pivottech/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         Restaurant myRestaurant = new Restaurant("Ruth Chris", Stars.FIVE, Restaurant.$$$);
         Review myReview = new Review("Great!", "Jimmy", Stars.FIVE);
-        //test
+        //test test
 
         myRestaurant.addReview(myReview);
 
